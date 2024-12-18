2b8b843d16aacf3c66e8e0378f36007c7ce543ac
hanyi <hanyi@hanyi.com>
2024-12-18 08:54:00 +0800

    udp-pub-sub

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -51,34 +51,34 @@ typedef StaticTask_t osStaticThreadDef_t;
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-osThreadId_t uartSessionTaskHandle;
-uint32_t uartTaskBuffer[ 2048 ];
-osStaticThreadDef_t uartTaskControlBlock;
-const osThreadAttr_t uartSessionTask_attributes = {
-    .name = "uartSessionTask",
+osThreadId_t udpPubSessionTaskHandle;
+uint32_t udpPubTaskBuffer[ 2048 ];
+osStaticThreadDef_t udpPubTaskControlBlock;
+const osThreadAttr_t udpPubSessionTask_attributes = {
+    .name = "udpPubSessionTask",
     .priority = (osPriority_t)osPriorityHigh,
-    .cb_mem = &uartTaskControlBlock,
-    .cb_size = sizeof(uartTaskControlBlock),
-    .stack_mem = &uartTaskBuffer[0],
-    .stack_size = sizeof(uartTaskBuffer),
+    .cb_mem = &udpPubTaskControlBlock,
+    .cb_size = sizeof(udpPubTaskControlBlock),
+    .stack_mem = &udpPubTaskBuffer[0],
+    .stack_size = sizeof(udpPubTaskBuffer),
 };
-osThreadId_t udpSessionTaskHandle;
-uint32_t udpTaskBuffer[ 2048 ];
-osStaticThreadDef_t udpTaskControlBlock;
-const osThreadAttr_t udpSessionTask_attributes = {
-    .name = "udpSessionTask",
+osThreadId_t udpSubSessionTaskHandle;
+uint32_t udpSubTaskBuffer[ 2048 ];
+osStaticThreadDef_t udpSubTaskControlBlock;
+const osThreadAttr_t udpSubSessionTask_attributes = {
+    .name = "udpSubSessionTask",
     .priority = (osPriority_t)osPriorityHigh,
-    .cb_mem = &udpTaskControlBlock,
-    .cb_size = sizeof(udpTaskControlBlock),
-    .stack_mem = &udpTaskBuffer[0],
-    .stack_size = sizeof(udpTaskBuffer),
+    .cb_mem = &udpSubTaskControlBlock,
+    .cb_size = sizeof(udpSubTaskControlBlock),
+    .stack_mem = &udpSubTaskBuffer[0],
+    .stack_size = sizeof(udpSubTaskBuffer),
 };
 
 osSemaphoreId_t semaphoreHandle;
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
-uint32_t defaultTaskBuffer[ 256 ];
+uint32_t defaultTaskBuffer[ 2048 ];
 osStaticThreadDef_t defaultTaskControlBlock;
 const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
@@ -92,7 +92,8 @@ const osThreadAttr_t defaultTask_attributes = {
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 void uartSessionTask(void *argument);
-void udpSessionTask(void *argument);
+void udpSubSessionTask(void *argument);
+void udpPubSessionTask(void *argument);
 
 /* USER CODE END FunctionPrototypes */
 
@@ -135,8 +136,9 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
-  uartSessionTaskHandle = osThreadNew(uartSessionTask, NULL, &uartSessionTask_attributes);
-  // udpSessionTaskHandle = osThreadNew(udpSessionTask, NULL, &udpSessionTask_attributes);
+  // uartSessionTaskHandle = osThreadNew(uartSessionTask, NULL, &uartSessionTask_attributes);
+  udpSubSessionTaskHandle = osThreadNew(udpSubSessionTask, NULL, &udpSubSessionTask_attributes);
+  udpPubSessionTaskHandle = osThreadNew(udpPubSessionTask, NULL, &udpPubSessionTask_attributes);
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
@@ -173,44 +175,147 @@ void StartDefaultTask(void *argument)
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
-void uartSessionTask(void *argument)
+void on_topic(
+        uxrSession* session,
+        uxrObjectId object_id,
+        uint16_t request_id,
+        uxrStreamId stream_id,
+        struct ucdrBuffer* ub,
+        uint16_t length,
+        void* args)
 {
-  osSemaphoreAcquire(semaphoreHandle, osWaitForever);
-  // osThreadTerminate(defaultTaskHandle);
-  session_info_t info;
-  if (usart_session_open(&info, huart1) < 0)
-  {
-    printf("usart session open error!");
-    HAL_GPIO_TogglePin(LD_USER1_GPIO_Port, LD_USER1_Pin);
-  }
-  osDelay(1000);
-  // Write topics
-  uint32_t count = 0;
-  /* Infinite loop */
-  while (count < 20)
-  {
-    HelloWorld topic = {
-        ++count, "Hello DDS world!"};
+    (void) session; (void) object_id; (void) request_id; (void) stream_id; (void) length;
+    // if(request_id != pubreqid) {
+    //     return;
+    // }
+    HelloWorld topic;
+    HelloWorld_deserialize_topic(ub, &topic);
 
-    ucdrBuffer ub;
-    uint32_t topic_size = HelloWorld_size_of_topic(&topic, 0);
-    uxr_prepare_output_stream(&(info.session), info.reliable_out, info.datawriter_id, &ub, topic_size);
-    HelloWorld_serialize_topic(&ub, &topic);
-    if(!uxr_run_session_time(&(info.session), 1000)){
-      HAL_GPIO_TogglePin(LD_USER1_GPIO_Port, LD_USER1_Pin);
-    }
-  }
-  // Delete resources
-  uxr_delete_session(&(info.session));
-  uxr_close_custom_transport(&(info.transport));
+//
+//    uint32_t* count_ptr = (uint32_t*) args;
+//    (*count_ptr)++;
 }
 
-void udpSessionTask(void *argument)
+
+void udpSubSessionTask(void *argument)
 {
   osSemaphoreAcquire(semaphoreHandle, osWaitForever);
-  char* ip = "10.2.25.98";
+  // State
+  char* ip = "192.168.1.3";
+  char* port = "1235";
+
+  // Transport
+  uxrUDPTransport transport;
+  if (!uxr_init_udp_transport(&transport, UXR_IPv4, ip, port))
+  {
+      // printf("Error at create transport.\n");
+      while (1)
+      {
+        /* code */
+      }
+      
+  }
+
+  // Session
+  uxrSession session;
+  uxr_init_session(&session, &transport.comm, 0xCCCCDDDD);
+  uxr_set_topic_callback(&session, on_topic, NULL);
+  if (!uxr_create_session(&session))
+  {
+      // printf("Error at create session.\n");
+      while (1)
+      {
+        /* code */
+      }
+  }
+
+  // Streams
+  uint8_t output_reliable_stream_buffer[BUFFER_SIZE];
+  uxrStreamId reliable_out = uxr_create_output_reliable_stream(&session, output_reliable_stream_buffer, BUFFER_SIZE,
+                  STREAM_HISTORY);
+
+  uint8_t input_reliable_stream_buffer[BUFFER_SIZE];
+  uxrStreamId reliable_in = uxr_create_input_reliable_stream(&session, input_reliable_stream_buffer, BUFFER_SIZE,
+                  STREAM_HISTORY);
+
+  // Create entities
+  uxrObjectId participant_id = uxr_object_id(0x02, UXR_PARTICIPANT_ID);
+  const char* participant_xml = "<dds>"
+          "<participant>"
+          "<rtps>"
+          "<name>subscribe_xrce_participant</name>"
+          "</rtps>"
+          "</participant>"
+          "</dds>";
+  uint16_t participant_req = uxr_buffer_create_participant_xml(&session, reliable_out, participant_id, 0,
+                  participant_xml, UXR_REUSE);
+
+  uxrObjectId topic_id = uxr_object_id(0x02, UXR_TOPIC_ID);
+  const char* topic_xml = "<dds>"
+          "<topic>"
+          "<name>HelloWorldTopic</name>"
+          "<dataType>HelloWorld</dataType>"
+          "</topic>"
+          "</dds>";
+  uint16_t topic_req = uxr_buffer_create_topic_xml(&session, reliable_out, topic_id, participant_id, topic_xml,
+                                                    UXR_REUSE);
+
+  uxrObjectId subscriber_id = uxr_object_id(0x02, UXR_SUBSCRIBER_ID);
+  const char* subscriber_xml = "";
+  uint16_t subscriber_req = uxr_buffer_create_subscriber_xml(&session, reliable_out, subscriber_id, participant_id,
+                  subscriber_xml, UXR_REUSE);
+
+  uxrObjectId datareader_id = uxr_object_id(0x02, UXR_DATAREADER_ID);
+  const char* datareader_xml = "<dds>"
+          "<data_reader>"
+          "<topic>"
+          "<kind>NO_KEY</kind>"
+          "<name>HelloWorldTopic</name>"
+          "<dataType>HelloWorld</dataType>"
+          "</topic>"
+          "</data_reader>"
+          "</dds>";
+  uint16_t datareader_req = uxr_buffer_create_datareader_xml(&session, reliable_out, datareader_id, subscriber_id,
+                  datareader_xml, UXR_REUSE);
+
+  // Send create entities message and wait its status
+  uint8_t status[4];
+  uint16_t requests[4] = {
+      participant_req, topic_req, subscriber_req, datareader_req
+  };
+  if (!uxr_run_session_until_all_status(&session, 1000, requests, status, 4))
+  {
+      printf("Error at create entities2: participant: %i topic: %i subscriber: %i datareader: %i\n", status[0],
+              status[1], status[2], status[3]);
+      // return 1;
+  }
+
+  // Request topics
+  uxrDeliveryControl delivery_control = {
+      0
+  };
+  delivery_control.max_samples = UXR_MAX_SAMPLES_UNLIMITED;
+  uint16_t request_id = uxr_buffer_request_data(&session, reliable_out, datareader_id, reliable_in, &delivery_control);
+  // printf("sub:request id:%d\n", request_id);
+  // Read topics
+  bool connected = true;
+  while (connected)
+  {
+      connected = uxr_run_session_time(&session, 1000);
+  }
+
+  // Delete resources
+  uxr_delete_session(&session);
+  uxr_close_udp_transport(&transport);
+
+}
+
+void udpPubSessionTask(void *argument)
+{
+  osSemaphoreAcquire(semaphoreHandle, osWaitForever);
+  char* ip = "192.168.1.3";
   char* port = "1234";
-  uint32_t max_topics = 15;
+  uint32_t max_topics = 20;
 
   // Transport
   uxrUDPTransport transport;
@@ -298,9 +403,14 @@ void udpSessionTask(void *argument)
   uint32_t count = 0;
   while (connected && count < max_topics)
   {
-      HelloWorld topic = {
-          ++count, "Hello DDS world!"
-      };
+      HelloWorld topic = {};
+      if(count %2 == 0) {
+        topic.index = ++count;
+        memcpy(topic.message, "LED_ON\0", 7);
+      } else {
+        topic.index = ++count;
+        memcpy(topic.message, "LED_OFF", 7);
+      }
 
       ucdrBuffer ub;
       uint32_t topic_size = HelloWorld_size_of_topic(&topic, 0);
